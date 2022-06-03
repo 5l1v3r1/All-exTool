95f3e4736606131a77d2d11e55eb395513212b65
AeX03 <103602164+AeX03@users.noreply.github.com>
2022-06-03 08:13:30 -0700

    Add files via upload

---
+++ Update.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+print ("""                                                                                                                                                                    
+                                                                                                                                                                    
+               AAA               llllllllllllll                                                        TTTTTTTTTTTTTTTTTTTTTTT                              lllllll 
+              A:::A              l:::::ll:::::l                                                        T:::::::::::::::::::::T                              l:::::l 
+             A:::::A             l:::::ll:::::l                                                        T:::::::::::::::::::::T                              l:::::l 
+            A:::::::A            l:::::ll:::::l                                                        T:::::TT:::::::TT:::::T                              l:::::l 
+           A:::::::::A            l::::l l::::l                      eeeeeeeeeeee  xxxxxxx      xxxxxxxTTTTTT  T:::::T  TTTTTTooooooooooo     ooooooooooo    l::::l 
+          A:::::A:::::A           l::::l l::::l                    ee::::::::::::ee x:::::x    x:::::x         T:::::T      oo:::::::::::oo oo:::::::::::oo  l::::l 
+         A:::::A A:::::A          l::::l l::::l                   e::::::eeeee:::::eex:::::x  x:::::x          T:::::T     o:::::::::::::::o:::::::::::::::o l::::l 
+        A:::::A   A:::::A         l::::l l::::l  --------------- e::::::e     e:::::e x:::::xx:::::x           T:::::T     o:::::ooooo:::::o:::::ooooo:::::o l::::l 
+       A:::::A     A:::::A        l::::l l::::l  -:::::::::::::- e:::::::eeeee::::::e  x::::::::::x            T:::::T     o::::o     o::::o::::o     o::::o l::::l 
+      A:::::AAAAAAAAA:::::A       l::::l l::::l  --------------- e:::::::::::::::::e    x::::::::x             T:::::T     o::::o     o::::o::::o     o::::o l::::l 
+     A:::::::::::::::::::::A      l::::l l::::l                  e::::::eeeeeeeeeee     x::::::::x             T:::::T     o::::o     o::::o::::o     o::::o l::::l 
+    A:::::AAAAAAAAAAAAA:::::A     l::::l l::::l                  e:::::::e             x::::::::::x            T:::::T     o::::o     o::::o::::o     o::::o l::::l 
+   A:::::A             A:::::A   l::::::l::::::l                 e::::::::e           x:::::xx:::::x         TT:::::::TT   o:::::ooooo:::::o:::::ooooo:::::ol::::::l
+  A:::::A               A:::::A  l::::::l::::::l                  e::::::::eeeeeeee  x:::::x  x:::::x        T:::::::::T   o:::::::::::::::o:::::::::::::::ol::::::l
+ A:::::A                 A:::::A l::::::l::::::l                   ee:::::::::::::e x:::::x    x:::::x       T:::::::::T    oo:::::::::::oo oo:::::::::::oo l::::::l
+AAAAAAA                   AAAAAAAlllllllllllllll                     eeeeeeeeeeeeeexxxxxxx      xxxxxxx      TTTTTTTTTTT      ooooooooooo     ooooooooooo   llllllll
+                                                                                                                                                                    
+                                                                                                                                                                    
+                                                                                                                                                                    
+                                                                                                                                                                    
+                                                                                                                                                                    
+                                                                                                                                                                    
+                                                                        ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗██╗███╗   ██╗ ██████╗ 
+                                                                        ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██║████╗  ██║██╔════╝ 
+                                                                        ██║   ██║██████╔╝██║  ██║███████║   ██║   ██║██╔██╗ ██║██║  ███╗
+                                                                        ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██║██║╚██╗██║██║   ██║
+                                                                        ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ██║██║ ╚████║╚██████╔╝
+                                                                         ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝ 
+                                                                                                                                                            """)
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/All-exTool/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/All-exTool
+
+clear
+
+mkdir All-exTool
+
+clear
+
+cd All-exTool
+
+clear
+
+git clone https://github.com/AeX03/All-exTool
+
+clear
+
+cd All-exTool
+
+clear
